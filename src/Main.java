f5cfa3b7e366b65a72e8b86f489d8ebad0faf63a
이은정 <ej2grateful@gmail.com>
2025-06-04 09:51:49 +0900

    연습2

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,10 @@
-//2025.06.04 논리연산자 연습
 public class Main {
     public static void main(String[] args) {
-        int age = 33;
 
-        boolean hasID = true;
+        int number = 5;
 
-        if(age >= 18 && hasID) {
-            System.out.println("접속가능.");
-        }
-
-        boolean isWeekend = false;
-        boolean isHoliday = true;
-
-        if(isWeekend || isHoliday) {
-            System.out.println("쉼");
-        }
-
-        boolean isRaining = true;
-        if(!isRaining) {
-            System.out.println("해");
-        } else {
-            System.out.println("비");
+        for (int i = 1; i <= 9 ; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
         }
     }
 }
